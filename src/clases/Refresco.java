e8b452eda7561538e30c5e0b829031bd994e3956
Usuario <Usuario@DESKTOP-124P0FI.home>
2023-01-10 21:52:13 +0100

    - clase refresco definida

---
+++ src/clases/Refresco.java
@@ -0,0 +1,47 @@
+package clases;
+
+public class Refresco extends Articulo{
+	
+	private String sabor;
+	private boolean zumo = false;
+	private boolean gaseoso = false;
+	private int cantidadAzucar;
+	
+	public boolean esSaludable () {
+		return false;
+	}
+
+	public String getSabor() {
+		return sabor;
+	}
+
+	public void setSabor(String sabor) {
+		this.sabor = sabor;
+	}
+
+	public boolean isZumo() {
+		return zumo;
+	}
+
+	public void setZumo(boolean zumo) {
+		this.zumo = zumo;
+	}
+
+	public boolean isGaseoso() {
+		return gaseoso;
+	}
+
+	public void setGaseoso(boolean gaseoso) {
+		this.gaseoso = gaseoso;
+	}
+
+	public int getCantidadAzucar() {
+		return cantidadAzucar;
+	}
+
+	public void setCantidadAzucar(int cantidadAzucar) {
+		this.cantidadAzucar = cantidadAzucar;
+	}
+	
+	
+}
