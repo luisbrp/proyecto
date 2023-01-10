1f840487852b403ff11ff212bdcc0bda141d1bdd
Usuario <Usuario@DESKTOP-124P0FI.home>
2023-01-10 21:49:31 +0100

    - Interfaz alcoholico definida.

---
+++ src/clases/Alcoholico.java
@@ -0,0 +1,15 @@
+package clases;
+
+public interface Alcoholico {
+	public static final double TASA_BEDIDAS_FUERTES = 64.13;
+	public static final double TASA_BEDIDAS_SUAVES = 38.48;
+	
+	public static boolean esFuerte () {
+		return false;
+	}
+	
+	public static double calcularTasa ( ) {
+		return 0;
+		
+	}
+}
